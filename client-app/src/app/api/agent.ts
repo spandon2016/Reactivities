45302f13f8dbf45bec8147ec97fbc4e3cd891ada
spandon2016 <spandon@gmail.com>
2022-12-02 17:43:00 -0500

    End of Section 17

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -9,6 +9,8 @@ import { history } from "../..";
 import { isJSDocDeprecatedTag } from "typescript";
 import { store } from "../stores/store";
 import { User, UserFormValues } from "../models/user";
+import { Photo, Profile } from "../models/profile";
+import { Form } from "semantic-ui-react";
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -90,10 +92,23 @@ const Account = {
     register: (user: UserFormValues) => requests.post<User>('/account/register', user)
 }
 
+const Profiles = {
+    get: (username: string) => requests.get<Profile>(`profiles/${username}`),
+    uploadPhoto: (file: Blob) => {
+        let formData =new FormData();
+        formData.append('File', file);
+        return axios.post<Photo>('photos', formData, {
+            headers: {'Content-type': 'multipart/form-data'}
+        })
+    },
+    setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
+    deletePhoto: (id: string) => requests.del(`/photos/${id}`)
+}
 
 const agent = {
     Activities,
-    Account
+    Account,
+    Profiles
 }
 
 // 147 , 10 s
